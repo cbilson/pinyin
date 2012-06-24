dc0c25a25d5c22e225a1f3108f30f68c7ba74c3e
Chris Bilson <cbilson@pobox.com>
2012-06-23 22:35:29 -0700

    fixup project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pinyin "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure wrapper for pinyin4j"
+  :url "http://github.com/cbilson/pinyin"
   :license {:name "GNU Public License v2"
             :url "http://www.gnu.org/licenses/gpl-2.0.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
