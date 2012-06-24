0230d1d2791256fa4f8383d43da7cc482e4cfd80
Chris Bilson <cbilson@pobox.com>
2012-06-23 22:30:25 -0700

    fix dependency to use by clojars fork of pinyin4j

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU Public License v2"
             :url "http://www.gnu.org/licenses/gpl-2.0.html"}
-  :repositories {"local" ~(str (.toURI (java.io.File. ".")))}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [net.sourceforge.pinyin4j "2.5.0"]])
+                 [org.clojars.cbilson/pinyin4j "2.5.0"]])
